4e49bcf61f90ba90b6a6cb362221abfc3f80da1f
SriBalajiMandala <sribalajimandala@gmail.com>
2026-07-15 18:12:08 +0530

    Time: 4 ms (13.03%), Space: 12.4 MB (16.77%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -4,15 +4,6 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        # while num>=10:
-        #     s=0
-        #     d=0
-        #     while num>0:
-        #         d=num%10
-        #         s+=d
-        #         num//=10
-        #     num=s
-        # return num
         while num>=10:
             s=0
             for i in str(num):
