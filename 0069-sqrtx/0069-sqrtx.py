c3a1f9a7ddc095d2c1da96dcb71de2678a6cfc30
Sri Balaji Mandala <sribalajimandala@gmail.com>
2026-07-16 12:16:21 +0530

    Time: 3 ms (66.1%), Space: 19.3 MB (57.84%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,3 +1,16 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x**0.5)
+       # return int(x**0.5)
+       l=0
+       h=x
+       a=0
+       while l<=h:
+            m=(l+h)//2
+            if m*m==x:
+                return m
+            elif m*m<x:
+                a=m
+                l=m+1
+            else:
+                h=m-1
+       return a
