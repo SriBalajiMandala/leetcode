1899ed0f041396b7dcf590e936904918acb4e4ac
Sri Balaji Mandala <sribalajimandala@gmail.com>
2026-07-20 11:48:21 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.35%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
@@ -1,10 +1,10 @@
 class Solution:
     def bitwiseComplement(self, n: int) -> int:
         l=bin(n)[2:]
-        compliment=""
+        comp=""
         for i in l:
             if i=="0":
-                compliment+="1"
+                comp+="1"
             else:
-                compliment+="0"
-        return int(compliment,2)
+                comp+="0"
+        return int(comp,2)
