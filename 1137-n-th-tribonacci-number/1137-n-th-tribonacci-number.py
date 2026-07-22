96738a2cbfda17d2ac2969518286ff64280e82ab
Sri Balaji Mandala <sribalajimandala@gmail.com>
2026-07-22 19:31:32 +0530

    Time: 0 ms (100%), Space: 19.3 MB (53.56%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -2,9 +2,7 @@ class Solution:
     def tribonacci(self, n: int) -> int:
         if n<=1:
             return n
-        a=0
-        b=1
-        c=1
+        a,b,c=0,1,1
         for _ in range(2,n):
             a,b,c=b,c,a+b+c
         return c
