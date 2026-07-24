83dc2772270071f773a120cca8e01bfcbdd7fa17
Sri Balaji Mandala <sribalajimandala@gmail.com>
2026-07-24 21:52:46 +0530

    Time: 0 ms (100%), Space: 19.4 MB (6.1%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        s=set()
+        while n!=0 and n not in s:
+            s.add(n)
+            n=sum(int(i)**2 for i in str(n))
+        return n==1
