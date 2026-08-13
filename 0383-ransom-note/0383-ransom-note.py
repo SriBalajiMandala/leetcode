e55d76711c6933c8b27b2f377da751eab9e890e5
Sri Balaji Mandala <sribalajimandala@gmail.com>
2026-08-13 11:03:45 +0530

    Time: 4 ms (95.15%), Space: 19.7 MB (8.53%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -5,14 +5,20 @@ class Solution:
         #         return True
         #     else:
         #         return False
-        s=""
-        for i in range(len(ransomNote)):
-            if ransomNote[i] in magazine:
-                s+=ransomNote[i]
-                magazine=magazine.replace(ransomNote[i],"",1)
-        if s==ransomNote:
-            return True
-        else:
-            return False
+        # s=""
+        # for i in range(len(ransomNote)):
+        #     if ransomNote[i] in magazine:
+        #         s+=ransomNote[i]
+        #         magazine=magazine.replace(ransomNote[i],"",1)
+        # if s==ransomNote:
+        #     return True
+        # else:
+        #     return False
+        for i in ransomNote:
+            if i in magazine:
+                magazine=magazine.replace(i,"",1)
+            else:
+                return False
+        return True
 
 
